2f94127706c680e2f4d89f17d5c33b764ac864a5
ankit <90479290+0205Ankit@users.noreply.github.com>
2023-08-23 18:15:21 +0530

    configuring store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,17 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeBuilder } from './themeProvider.tsx'
+import { Provider } from 'react-redux'
+import store from './store/store.ts'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeBuilder>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeBuilder>
+    <Provider store={store}>
+      <ThemeBuilder>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeBuilder>
+    </Provider>
   </React.StrictMode>
 );
